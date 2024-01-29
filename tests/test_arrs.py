146ad6bfffc714152f48819fd8f180e6aeb58cb9
Aleksey Bondarenko <bondaralexcy.gmail.com>
2024-01-29 23:25:31 +0300

    Additional Changes to test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,7 +1,6 @@
 import unittest
 from utils import arrs
 
-
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
@@ -9,6 +8,8 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([], 0, "test"), "test")
         self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
         self.assertEqual(arrs.get([1, 2, 3], 5, "test"), "test")
+
+
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
